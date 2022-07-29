f506b9c9c74c4a6b0353fa8eb527af16fa376fd9
Eliezer Miguel <eliezermiguel97@outlook.com>
2022-07-29 20:22:10 -0300

    Quick Sort

---
+++ arrays.js
@@ -0,0 +1,50 @@
+const edFolha = [
+    {
+        titulo: "PHP",
+        preco: 15
+    }, 
+    {
+        titulo: "JavaScript",   
+        preco: 25
+    },
+    {
+        titulo: "Java",
+        preco: 30
+    }, 
+    {
+        titulo: "Go",
+        preco: 45
+    }, 
+    {
+        titulo: "Elixir",
+        preco: 50
+    } 
+]
+const edGalho = [
+    {
+        titulo: "Python",
+        preco: 20
+    },
+    {
+        tiulo: "Rust",
+        preco: 22
+    },
+    {
+        tiulo: "Ruby",
+        preco: 28
+    },
+    {
+        tiulo: "C#",
+        preco: 33
+    },
+    {
+        tiulo: "C++",
+        preco: 35
+    },
+    {
+        tiulo: "Scala",
+        preco: 40
+    }
+]
+
+module.exports = {edFolha, edGalho};
